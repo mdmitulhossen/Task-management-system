6fedb5aaf1371f5b56ea15411e4b60880d06f21b
Md Mitul Hossain <md.mitul.hossen99@gmail.com>
2023-12-22 04:18:33 +0600

    feature services secction done

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -14,8 +14,8 @@ const Hero = () => {
             }}
             className="w-full lg:h-[650px] md:h-[500px]"
         >
-            <div className='taskContainer border h-full border-red-500 flex gap-4 justify-between items-center flex-wrap md:flex-nowrap'>
-                <div className='space-y-4 w-full md:flex-1 pt-28 md:pt-0 flex  flex-col justify-center items-center'>
+            <div className='taskContainer  h-full  flex gap-4 justify-between items-center flex-wrap md:flex-nowrap'>
+                <div className='space-y-4 w-full md:flex-1 pt-28 md:pt-0 flex  flex-col justify-center items-center md:items-start'>
                     <p className='text-[#1E1C24] text-[32px] md:text-[36px] lg:text-[44px] xl:text-[55px] font-bold text-center md:text-left'>Task management <br /> and lists Tool</p>
                     <p className='text-[#b1aaaa] md:text-[16px] lg:text-[20px] text-center md:text-left'>We believe that designing products and services in close partnership with our clients is the only way to have a real impact on their business.</p>
                     <button className='px-5 py-3 font-bold text-sm rounded bg-[#525FFB] text-white hover:bg-[#313ba3] duration-200 '>Let’s Explore</button>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,47 @@
 import Hero from "./Hero";
-
+import homeBg1 from '../../assets/home1.png'
+import serviceicon1 from '../../assets/icons/service1.png'
+import serviceicon2 from '../../assets/icons/service2.png'
+import serviceicon3 from '../../assets/icons/service3.png'
 
 const Home = () => {
     return (
         <div>
-           <Hero/>
+            <Hero />
+
+            {/* /feature services section */}
+            <div
+                style={{
+                    backgroundImage: `url(${homeBg1})`,
+                    // height: "100vh",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+
+                }}
+                className="p-20"
+            >
+                <p className="text-[#1e1c24] lg:text-[45px] md:text-[40px] text-[32px] xl:text-[50px] text-center font-semibold">Featured Services</p>
+                <div className="flex justify-center md:gap-10 xl:gap-16 gap-5 flex-wrap mt-16">
+                    <div className="min-w-[280px] w-[280px] p-5 bg-[#525FFB] shadow-inner rounded flex flex-col justify-center items-center gap-3">
+                        <img className="w-[100px]" src={serviceicon1} alt="" />
+                        <p className="text-white font-bold text-[24px] text-center">Keep tasks in
+                            one place</p>
+                        <p className="text-white/60 text-center">Save time, avoid losing work and information, delegate, and track tasks to stay on schedule</p>
+                    </div>
+                    <div className="min-w-[280px] w-[280px] p-5 bg-[#858ff6] shadow-inner rounded flex flex-col justify-center items-center gap-3 md:mt-10">
+                        <img className="w-[100px]" src={serviceicon2} alt="" />
+                        <p className="text-white font-bold text-[24px] text-center">Manage tasks in
+                            one place</p>
+                        <p className="text-white/60 text-center">Save time, avoid losing work and information, delegate, and track tasks to stay on schedule</p>
+                    </div>
+                    <div className="min-w-[280px] w-[280px] p-5 bg-[#525FFB] shadow-inner rounded flex flex-col justify-center items-center gap-3">
+                        <img className="w-[100px]" src={serviceicon3} alt="" />
+                        <p className="text-white font-bold text-[24px] text-center">Drag and Drop</p>
+                        <p className="text-white/60 text-center">Save time, avoid losing work and information, delegate, and track tasks to stay on schedule</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
